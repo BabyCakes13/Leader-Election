87f75fe87f66617924cea5baed6092fb7a193ca9
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2020-01-21 17:48:00 +0200

    Created the basic structure of the leader election algorithm.

---
+++ election_client.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import socket
+import time
+import sys
+import logging
+
+
+class LeaderElectionClient:
+    TCP_PORT = 5001  # change port based on other implemented features
+    BUFFER_SIZE = 1024
+    HEARTBEAT_INCREMENT = 1
+
+    def __init__(self):
+        self.attribute = 1  # TODO find a good attribute
+        self.server_list = sys.argv[1:]  # get the servers which the client needs to connect to
+
+        logging.basicConfig(format='%(levelname)s: %(asctime)s %(message)s', level=logging.INFO)
+
+    def start_client(self):
+        self.set_connection()
+
+        while 1:
+            time.sleep(100)
+
+    def set_connection(self):
+
+        for server in self.server_list:
+            logging.debug("INITIALISING CONNECTION TO " + str(server))
+            try:
+                self.send_message(server=server)
+            except ConnectionRefusedError:
+                print("CONNECTION REFUSED")
+
+    def send_message(self, server):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((server, self.TCP_PORT))
+            s.send(int.to_bytes(1, byteorder="little", length=8))
+            s.close()
+
+        except ConnectionAbortedError:
+            print("CONNECTION ABORTED.")
+
+
+if __name__ == "__main__":
+    client = LeaderElectionClient()
+    client.start_client()
